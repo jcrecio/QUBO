b5b95af1a7e7dfca49244dace8e1a12d23851998
jcrecio <juan.carlos.recio.abad@gmail.com>
2022-12-22 10:56:29 +0100

    Added plots

--- src/main/java/es/uma/informatica/misia/ae/simpleea/Main.java
+++ src/main/java/es/uma/informatica/misia/ae/simpleea/Main.java
@@ -1,8 +1,17 @@
 package es.uma.informatica.misia.ae.simpleea;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.github.sh0nk.matplotlib4j.NumpyUtils;
+import com.github.sh0nk.matplotlib4j.Plot;
+import com.github.sh0nk.matplotlib4j.PythonExecutionException;
+
 public class Main {
 
-	private static final int ITERATIONS = 30;
+	private static final int ITERATIONS = 1;
 	private static final int INSTANCES = 10;
 	
 	public static void main (String args []) {
@@ -16,19 +25,22 @@ public class Main {
 			return;
 		}
 		
+		double[] graphPopulation = new double[INSTANCES], graphMutation = new double[INSTANCES];
+		
 		int n = Integer.parseInt(args[3]);
 		int parameter = Integer.parseInt(args[4]);
 		
 		// QUBO instances
 		for (int quboSeed = 0; quboSeed < INSTANCES; quboSeed++) {
-			Problem problem = new Qubo(n, quboSeed);
+			Qubo problem = new Qubo(n, quboSeed);
+
 			double accumulatedMutationRate = 0, standardDeviationMutationRate = 0;
 			double accumulatedPopulation = 0, standardDeviationPopulation = 0;
 			
 			double[] historyMutation = new double[ITERATIONS];
 			double[] historyPopulation = new double[ITERATIONS];
 			
-			// 30 executions for each instance
+			// executions for each instance
 			for (int executionSeed = 0; executionSeed < ITERATIONS; executionSeed++) {
 				double[] arguments = resetArguments(args);
 				double[] results = runExperiment(problem, arguments, executionSeed, parameter);
@@ -50,13 +62,18 @@ public class Main {
 			
 			standardDeviationMutationRate = Math.sqrt(standardDeviationMutationRate / ITERATIONS);
 			standardDeviationPopulation = Math.sqrt(standardDeviationPopulation / ITERATIONS);
-			
-			System.out.println("Instance QUBO[" + quboSeed + "] after 30 executions.");
+					
+			System.out.println("Instance QUBO[" + quboSeed + "] after " + ITERATIONS +  " executions.");
 			System.out.println("Average optimal mutation rate = " + avgMutationRate);
 			System.out.println("Average optimal population size = " + avgPopulation);
 			System.out.println("Standard deviation mutation rate = " + standardDeviationMutationRate);
 			System.out.println("Standard deviation population size = " + standardDeviationPopulation);
+		
+			graphPopulation[quboSeed] = avgPopulation;
+			graphMutation[quboSeed] = avgMutationRate;
 		}
+		
+		plot(graphMutation, graphPopulation);
 	}
 	
 	private static double[] runExperiment(Problem problem, double[] arguments, long seed, int parameter){
@@ -88,7 +105,7 @@ public class Main {
 		// Search Best population with fixed mutation rate
 		// Vary population from 100, 150, 200, 250... 20000 
 		Experiment experimentPopulation = 
-				new Experiment("population", "mutation", 100, 1000, 50, 2, arguments[2], seed);
+				new Experiment("population", "mutation", 10, 1000, 10, 0, arguments[2], seed);
 		return experimentPopulation.Run(problem, arguments);
 	}
 	
@@ -98,5 +115,37 @@ public class Main {
 			if (args.length > i+1) arguments[i] = Double.parseDouble(args[i]);
 		}
 		return arguments;
+	}
+	
+	private static void plot(double[] mutation, double[] population) {
+//		List<Double> x = NumpyUtils.linspace(10, 1000, 990);
+//		List<Double> y = x.stream()
+//				.map(xi -> mutation[(int) (Math.round(xi)-1)])
+//				.collect(Collectors.toList());
+//
+//		Plot plt = Plot.create();
+//		plt.plot().add(x, y, "o").label("Mutation").color("#66DD66");
+//		plt.legend().loc("upper right");
+//		plt.title("scatter");
+//		try {
+//			plt.show();
+//		} catch (IOException | PythonExecutionException e) {
+//			e.printStackTrace();
+//		}
+//		
+		List<Double> x = NumpyUtils.linspace(1, 10, 10);
+		List<Double> y = x.stream()
+				.map(xi -> mutation[(int) (Math.round(xi)-1)])
+				.collect(Collectors.toList());
+
+		Plot plt2 = Plot.create();
+		plt2.plot().add(x, y, "o").label("Population").color("#6688FF");
+		plt2.legend().loc("upper right");
+		plt2.title("scatter");
+		try {
+			plt2.show();
+		} catch (IOException | PythonExecutionException e) {
+			e.printStackTrace();
+		}
 	}
 }
